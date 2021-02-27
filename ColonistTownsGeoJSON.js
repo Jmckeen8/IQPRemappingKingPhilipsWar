204ae4941cf06170054d56b423a900ba2864c34f
Joshua McKeen <jdmckeen@wpi.edu>
2021-02-27 03:31:25 -0500

    initial load in of working maps (Colonist Towns v1 and Praying towns v2)

---
+++ ColonistTownsGeoJSON.js
@@ -0,0 +1,230 @@
+var colonistTowns = 
+	{
+		"type": "FeatureCollection",
+		"features": [
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-70.665833,
+						41.955556
+					]
+				},
+				"properties": {
+					"name": "Town of Plymouth",
+					"popupContent": "Sassamon's alleged murderers are executed - June 8, 1675"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.189722,
+						41.747778
+					]
+				},
+				"properties":{
+					"name": "Town of Swansea",
+					"popupContent": "Wampanoag's first attack on colonists - June 24, 1675"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-72.102222,
+						42.213611
+					]
+				},
+				"properties":{
+					"name": "Town of Brookfield",
+					"popupContent": "Attack on Brookfield - August 2-4, 1675"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-72.605556,
+						42.544444
+					]
+				},
+				"properties":{
+					"name": "Town of Deerfield",
+					"popupContent": "Attack on Deerfield - September 1-2, 1675"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.68683,
+						42.44163
+					]
+				},
+				"properties":{
+					"name": "Springfield",
+					"popupContent": "attack/destruction of Springfield - October 5, 1675"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.673056,
+						42.455278
+					]
+				},
+				"properties":{
+					"name": "Town of Lancaster",
+					"popupContent": "February 10, 1675 - raid on Lancaster"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.306111,
+						42.186944
+					]
+				},
+				"properties":{
+					"name": "Town of Medfield",
+					"popupContent": "February 21, 1675 - attack on Medfield"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.68683,
+						42.44163
+					]
+				},
+				"properties":{
+					"name": "Town of Sudbury",
+					"popupContent": "April 21, 1676 - attack on Sudbury"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.551994,
+						42.347724
+					]
+				},
+				"properties":{
+					"name": "Marlborough",
+					"popupContent": "Captured Tukapewillin and other Indians harrased and abused by English while trying to escape captivity from the Nipmucks - March 1676"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-70.663652,
+						41.957577
+					]
+				},
+				"properties":{
+					"name": "Plymouth Court House/Meeting House",
+					"popupContent": "Location of numorous interactions (of Legal nature) between Indians and Colonists"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.117722,
+						42.373966
+					]
+				},
+				"properties":{
+					"name": "Harvard College",
+					"popupContent": "John Sassamon attends Harvard College"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.065628,
+						42.355101
+					]
+				},
+				"properties":{
+					"name": "Boston",
+					"popupContent": "Public executions of captured Indians, occurred on what is now known as the Boston Common - August and September, 1676"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.905881,
+						41.335957
+					]
+				},
+				"properties":{
+					"name": "Stonington",
+					"popupContent": "Narragansett Sachem Canochet brough here after capture before being executed"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-70.956944,
+						41.597779
+					]
+				},
+				"properties":{
+					"name": "Dartmouth, Russell Garrison House",
+					"popupContent": "Wampanoag forces raid Dartmouth in an effort to divide the English forces, slowing their pursuit through Pocasset - mid-July 1675"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.535794,
+						42.09306
+					]
+				},
+				"properties":{
+					"name": "Mendon",
+					"popupContent": "Nipmuc forces raid Mendon - July 14, 1675"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.68683,
+						42.44163
+					]
+				},
+				"properties":{
+					"name": "Rowlandson Rock",
+					"popupContent": "Rock Mary Rowlandson and other captives passed by on their first night in captivity - historical marker - February 10-11, 1676"
+				}
+			}
+		]	
+	};
---
+++ PrayingTownsGeoJSON.js
@@ -0,0 +1,348 @@
+var prayingTowns = 
+	{
+		"type": "FeatureCollection",
+		"features": [
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.316111,
+						42.273056
+					]
+				},
+				"properties": {
+					"name": "The John Eliot Oak, Natick",
+					"popupContent": "John Eliot establishes a location to preach to and answer questions from the Praying Indians"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-70.880556,
+						41.906111
+					]
+				},
+				"properties":{
+					"name": "Namasket",
+					"popupContent": "Christian Indian town where John Sassamon served as minister"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.31605,
+						42.273
+					]
+				},
+				"properties":{
+					"name": "Natick Meetinghosue",
+					"popupContent": "Meetinghouse constructed by Christian Indians with support from John Eliot"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.68515,
+						42.2068
+					]
+				},
+				"properties":{
+					"name": "Hassanemesit",
+					"popupContent": "James Printer's hometown, attacked by Nipmucs in early November, 1675"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-70.955224,
+						42.348522
+					]
+				},
+				"properties":{
+					"name": "Deer Island",
+					"popupContent": "In October 1675 Praying Indians are forced to relocated to Deer Island"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.552322,
+						42.34724
+					]
+				},
+				"properties":{
+					"name": "Marlborough Meeting House",
+					"popupContent": "In 1663, Marlborough proceeded to build their meeting house in the Praying Indians' planting field, technically outside the boundaries of the colonial town."
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.302587,
+						42.645659
+					]
+				},
+				"properties":{
+					"name": "Wamesit Village",
+					"popupContent": "Described as along the river banks where the Concord River meets the Merrimack "
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.313364,
+						42.63881
+					]
+				},
+				"properties":{
+					"name": "Wamesit Meetinghouse Hill",
+					"popupContent": "Location of John Eliot's Meetinghouse to preach to the Wamesits"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.545872,
+						42.3731
+					]
+				},
+				"properties":{
+					"name": "Fort Meadow, Okkokonimesit",
+					"popupContent": "Used as a fishing outpost along the falls of Fort Meadow Brook. Used in winter to bury and secure the summer harvest"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.119249,
+						42.198913
+					]
+				},
+				"properties":{
+					"name": "Punkapog",
+					"popupContent": "Location of historical marker placed on the northern line of the area"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.481502,
+						42.252849
+					]
+				},
+				"properties":{
+					"name": "Makunkokoag",
+					"popupContent": "Marker placed on area known as Magunco Hill"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.454451,
+						42.510017
+					]
+				},
+				"properties":{
+					"name": "Sarah Doublet Forest, Nashoba",
+					"popupContent": "After the inhabitants of Nashoba were forced to Deer Island during the war, much of the village was parceled off for the colonists. The small part that remained for those who returned remains as the Sarah Doublet Forest"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.462759,
+						42.511979
+					]
+				},
+				"properties":{
+					"name": "Nashoba Indian Fort",
+					"popupContent": "Originally constructed to defend against Mohawk attacks, the exact location of the fort has been lost to history, but this point is one of the educated guesses that exist"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.803409,
+						42.119175
+					]
+				},
+				"properties":{
+					"name": "Manchaug, village",
+					"popupContent": "Gookin identified the location of the village on what is now Whittier's Hill"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.776736,
+						42.107431
+					]
+				},
+				"properties":{
+					"name": "Manchaug Pond",
+					"popupContent": "Evidence of Indian activity exists on north shore of Pond, closest to the village"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.862929,
+						42.045963
+					]
+				},
+				"properties":{
+					"name": "Chabunakonkomug",
+					"popupContent": "Location of historical marker. This praying town was established in 1674 and lasted only a year until the onset of King Philip's War"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.462759,
+						42.511979
+					]
+				},
+				"properties":{
+					"name": "Waeuntuk",
+					"popupContent": "Identified as 'east of the present centre of the town, on the plain between the mumford and West Rivers'"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.844633,
+						41.95595
+					]
+				},
+				"properties":{
+					"name": "Quantisset",
+					"popupContent": "This location, now known as 'Fort Hill' was the site of the Quantisset Village/Fort"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.940461,
+						42.01348
+					]
+				},
+				"properties":{
+					"name": "Maanexit",
+					"popupContent": "Located in what is now the village of Fabyan, Maanexit was located just east of the Quinebaug River"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Point",
+					"coordinates": [
+						-71.948941,
+						41.938289
+					]
+				},
+				"properties":{
+					"name": "Webquasset",
+					"popupContent": "Webquasset was the largest of the three Connecticut Praying Towns, located six miles west of the Quinebaug River and 'near the bend in the outlet from Woodstock Pond' which is now called Roseland Lake"
+				}
+			},
+			
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[-71.552836, 42.346750],
+							[-71.569633, 42.390802],
+							[-71.525184, 42.390539],
+							[-71.524718, 42.377222],
+							[-71.501744, 42.377684],
+							[-71.495788, 42.351980],
+							[-71.546089, 42.350293],
+							[-71.545387, 42.347466]
+						]
+					]
+				},
+				"properties":{
+					"name": "Okkokonimesit",
+					"popupContent": "Overall boundary of praying town of Okkokonimesit"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[-71.55276, 42.346615],
+							[-71.553801, 42.354379],
+							[-71.549129, 42.354789],
+							[-71.545861, 42.347136]
+
+						]
+					]
+				},
+				"properties":{
+					"name": "Okkokonimesit planting field",
+					"popupContent": "The Okkokonimesit Indians' primary planting field"
+				}
+			},
+			{
+				"type": "Feature",
+				"geometry": {
+					"type": "Polygon",
+					"coordinates": [
+						[
+							[-71.445616, 42.50869],
+							[-71.516120, 42.487081],
+							[-71.541415, 42.550438],
+							[-71.481329, 42.561561]
+
+						]
+					]
+				},
+				"properties":{
+					"name": "Nashoba",
+					"popupContent": "Overall boundary of praying town of Nashoba"
+				}
+			}
+		]	
+	};
+	
